b0da73205e9b04d64061bb67c87e99f263a8fb4c
INNOVINATI <maximilian.wolf@innovinati.com>
2020-12-19 15:06:48 +0100

    Added HTMLExporter and BaseExporter.domain

--- microwler/export.py
+++ microwler/export.py
@@ -11,7 +11,8 @@ logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(asctime)s %(me
 class BaseExporter:
     extension = ''
 
-    def __init__(self, data: list, settings: Settings):
+    def __init__(self, domain: str, data: list, settings: Settings):
+        self.domain = domain
         self.data = data
         self.settings = settings
 
@@ -21,7 +22,7 @@ class BaseExporter:
     def export(self):
         data = self.convert()
         timestamp = datetime.now().strftime('%Y-%m-%d-%H:%M')
-        path = os.path.join(self.settings.export_to, f'{timestamp}.{self.extension}')
+        path = os.path.join(self.settings.export_to, f'{self.domain}_{timestamp}.{self.extension}')
         try:
             if not os.path.exists(self.settings.export_to):
                 os.mkdir(self.settings.export_to)
@@ -47,9 +48,31 @@ class CSVExporter(BaseExporter):
         # Flatten the result dicts and throw away the links
         flat = map(lambda obj: {'url': obj['url'], 'depth': obj['depth'], **obj.get('data', {})}, self.data)
         data = list(flat)
-        headers = ';'.join([key for key in data[0].keys()])
-        rows = [';'.join([val for val in obj.values()]) for obj in data]
+        headers = ';'.join([key.upper() for key in data[0].keys()])
+        rows = '\n'.join([';'.join([str(val) for val in obj.values()]) for obj in data])
         table = '\n'.join([headers, rows])
         return table
+
+
+class HTMLExporter(BaseExporter):
+    extension = 'html'
+
+    def convert(self):
+        flat = map(lambda obj: {'url': obj['url'], 'depth': obj['depth'], **obj.get('data', {})}, self.data)
+        data = list(flat)
+        styles = 'width: 100%; border: 1px solid grey; text-align: center'
+        headers = ''.join([f'<th>{key.upper()}</th>' for key in data[0].keys()])
+        rows = ''.join([f"<tr>{''.join([f'<td>{val}</td>' for val in obj.values()])}</tr>" for obj in data])
+        table = '<!DOCTYPE html>' \
+                '<html>' \
+                '<body>' \
+                f'<table style="{styles}">' \
+                f'<tr>{headers}</tr>' \
+                f'<tbody>{rows}</tbody>' \
+                '</table>' \
+                '<body>' \
+                '</html>'
+        return table
+
 
 
