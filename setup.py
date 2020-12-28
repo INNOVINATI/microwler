318322e813c9a15763ba810731f8c3adad93f78a
Maximilian Wolf <maximiliancw@innovinati.com>
2020-12-28 21:36:13 +0100

    Updated package info and setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
@@ -7,24 +7,24 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='microwler',
-    version='0.1.0',
-    description='A micro-framework for deep crawls and scraping written in Python',
+    version='0.1.1',
+    description='A micro-framework for asynchronous deep crawls and web scraping written in Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://innovinati.github.io/microwler/',
     author='Maximilian Wolf',
     author_email='maximilian.wolf@innovinati.com',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
+        'Framework :: AsyncIO',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3 :: Only',
     ],
-    package_dir={'': 'microwler'},
-    packages=find_packages(where='microwler'),
+    packages=['microwler'],
     python_requires='>=3.6, <4',
     install_requires=['aiohttp', 'lxml', 'prettytable', 'random-user-agent', 'html-text'],
     project_urls={
