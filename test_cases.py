ef25616e4cb31a0d6b5fc6a1f6ee6f80a6046cc2
INNOVINATI <maximilian.wolf@innovinati.com>
2021-01-08 15:26:12 +0100

    Unify test cases

--- test_cases.py
+++ test_cases.py
@@ -1,6 +1,6 @@
 """
 As of v0.1.3, this file holds some more or less naive test cases,
-which can also be used as examples for using the framework.
+which can also be viewed as examples for using the framework.
 
 Some future version will have a more sophisticated test suite, i.e.
 by integrating tests for the webservice.
@@ -15,8 +15,6 @@ from microwler.export import JSONExporter, HTMLExporter
 def test_basic():
     crawler = Microwler('https://quotes.toscrape.com/')
     crawler.run(verbose=True)
-    for page in crawler.pages:
-        print(page.url, page.html)
 
 
 @pytest.mark.asyncio
@@ -33,10 +31,6 @@ def test_intermediate():
 
     crawler = Microwler('https://quotes.toscrape.com/', selectors=selectors, settings=settings)
     crawler.run(verbose=True)
-    for page_data in crawler.data:
-        print(f'\n{page_data["url"].upper()}')
-        for key, value in page_data.items():
-            print(f'> {key.upper()}: {value}')
 
 
 @pytest.mark.asyncio
