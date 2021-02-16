8fea66c9be1afef259c6912a88a0f68e7e334bfa
INNOVINATI <maximilian.wolf@innovinati.com>
2021-02-16 13:56:19 +0100

    Bump version; rename CLI command "new" to "create"

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='microwler',
-    version='0.1.7',
-    description='A micro-framework for asynchronous deep crawls and web scraping written in Python',
+    version='0.2.0',
+    description='A micro-framework for asynchronous deep crawls and web scraping with Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://innovinati.github.io/microwler/',
     author='Maximilian Wolf',
     author_email='maximilian.wolf@innovinati.com',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Framework :: AsyncIO',
         'License :: OSI Approved :: MIT License',
@@ -29,13 +29,13 @@ setup(
     python_requires='>=3.7, <4',
     install_requires=[
         'aiohttp', 'lxml', 'diskcache', 'prettytable', 'quart',
-        'random-user-agent', 'html-text', 'completely', 'click',
+        'random-user-agent', 'html-text', 'click',
         'parsel', 'chardet', 'quart-cors', 'aiodns'
     ],
     entry_points='''
     [console_scripts]
     microwler=microwler.cli.cmd:show_help
-    new=microwler.cli.cmd:add_project
+    create=microwler.cli.cmd:add_project
     crawler=microwler.cli.cmd:crawler
     serve=microwler.cli.cmd:start_server
     ''',
