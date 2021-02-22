17a08d2c13d2308ec75555df1d0e69506542d31b
Maximilian Wolf <maximilian.wolf@innovinati.com>
2021-02-22 16:38:15 +0100

    Implement test suite

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,19 +1,48 @@
 # TODO: https://pgjones.gitlab.io/quart/how_to_guides/testing.html
+import pytest
+
+from microwler.web.backend import app
 
 
-class TestStatus:
-    pass
+class TestWebservice:
 
+    @pytest.mark.asyncio
+    def setup_class(self):
+        self.client = app.test_client()
 
-class TestProject:
-    pass
+    @pytest.mark.asyncio
+    async def test_index(self):
+        response = await self.client.get('/')
+        assert response.status_code == 200
 
+    @pytest.mark.asyncio
+    async def test_status(self):
+        response = await self.client.get('/status')
+        assert response.status_code == 200
+        data = await response.get_json()
+        assert data
+        assert len(data['projects']) == 1
 
-class TestCrawl:
-    pass
+    @pytest.mark.asyncio
+    async def test_project_status(self):
+        response = await self.client.get('/status/quotes')
+        assert response.status_code == 200
+        data = await response.get_json()
+        assert data
+        assert data['name'] == 'quotes' and data['start_url'] == 'https://quotes.toscrape.com/'
 
+    @pytest.mark.asyncio
+    async def test_crawl(self):
+        response = await self.client.get('/crawl/quotes')
+        assert response.status_code == 200
+        data = await response.get_json()
+        assert data
+        assert len(data['data']) == 214
 
-class TestData:
-    pass
-
-
+    @pytest.mark.asyncio
+    async def test_cache(self):
+        response = await self.client.get('/data/quotes')
+        assert response.status_code == 200
+        data = await response.get_json()
+        assert data
+        assert len(data['data']) == 214
